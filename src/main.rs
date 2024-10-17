7ac56950a83f4aeffeb15febbcf62e7039063dc7
surajatreya <surajatreyac@gmail.com>
2024-10-17 10:29:05 +0530

    Removed tonic-reflection since it doesn't work with autometrics

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,72 @@
+use std::future::Future;
+use std::net::SocketAddr;
+use std::str::FromStr;
+use autometrics::autometrics;
+
+use prometheus::Encoder;
 use tonic::{Request, Response, Status};
-use tonic::transport::Server;
+use tonic::codegen::Body;
+use tonic::transport::{Server, server};
+use autometrics::prometheus_exporter::{self, PrometheusResponse};
+
+use axum::{routing::get, Router};
+use axum::routing::Route;
+use tokio::net::TcpListener;
 
 use proto::calculator_server::{Calculator, CalculatorServer};
 
-use crate::proto::{CalculationRequest, CalculationResponse, FILE_DESCRIPTOR_SET};
+use crate::proto::{CalculationRequest, CalculationResponse};
 
-mod proto{
+mod proto {
     tonic::include_proto!("calculator");
-
-    pub(crate) const FILE_DESCRIPTOR_SET: &[u8] =
-        tonic::include_file_descriptor_set!("calculator_descriptor");
 }
 
 #[derive(Debug, Default)]
-struct CalculatorService{}
+struct CalculatorService {}
 
 #[tonic::async_trait]
-impl Calculator for CalculatorService{
+impl Calculator for CalculatorService {
+    #[autometrics]
     async fn add(&self, request: Request<CalculationRequest>) -> Result<Response<CalculationResponse>, Status> {
         println!("Got a request: {:?}", request);
 
         let input = request.get_ref();
 
-        let response = CalculationResponse{
+        let response = CalculationResponse {
             result: input.a + input.b,
         };
         Ok(Response::new(response))
     }
 }
 
+pub fn get_metrics() -> PrometheusResponse {
+    prometheus_exporter::encode_http_response()
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "127.0.0.1:50051".parse()?;
-    let calc = CalculatorService::default();
+    prometheus_exporter::init();
+    eprintln!("Prometheus init");
+    let addr: SocketAddr = "127.0.0.1:50051".parse().unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();    let calc = CalculatorService::default();
 
-    let service = tonic_reflection::server::Builder::configure()
-        .register_encoded_file_descriptor_set(FILE_DESCRIPTOR_SET)
-        .build()
-        .unwrap();
+    tokio::spawn(async move {
+        Server::builder()
+            .add_service(CalculatorServer::new(calc))
+            .serve(addr.into())
+            .await
+            .unwrap();
+    });
 
-    Server::builder()
-        .add_service(service)
-        .add_service(CalculatorServer::new(calc))
-        .serve(addr)
-        .await?;
+    let app = Router::new()
+        .route("/", get(handler))
+        .route("/metrics", get(|| async { prometheus_exporter::encode_http_response() }));
+
+    axum::serve(listener, app).await.unwrap();
 
     Ok(())
+}
+
+async fn handler() -> &'static str {
+    "Hello, World!"
 }
